553e7129c2d5b2cc0a3674e65f64ccc65c304068
Valkær <au617767@post.au.dk>
2023-03-21 14:47:33 +0100

    Prepared for 2-edge Connectivity

---
+++ src/twoEdgeConnectivityTopTree.java
@@ -0,0 +1,26 @@
+public class twoEdgeConnectivityTopTree implements TopTreeInterface {
+    @Override
+    public void combine(Node t) {
+
+    }
+
+    @Override
+    public UserInfo computeCombine(Node t) {
+        return null;
+    }
+
+    @Override
+    public Node search(Node root) {
+        return null;
+    }
+
+    @Override
+    public UserInfo newUserInfo() {
+        return null;
+    }
+
+    @Override
+    public int combineCost(Node grandParent) {
+        return 0;
+    }
+}
---
+++ src/twoEdgeConnectivityUserInfo.java
@@ -0,0 +1,9 @@
+public class twoEdgeConnectivityUserInfo implements UserInfo {
+
+
+
+
+
+
+
+}
