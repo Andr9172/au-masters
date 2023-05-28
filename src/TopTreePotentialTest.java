723788bb904ca8cf3f23b098dfb8f5b5bb23a3fa
andr9172 <au617767@post.au.dk>
2023-05-28 17:58:28 +0200

    Potential tests done?

--- src/TopTreePotentialTest.java
+++ src/TopTreePotentialTest.java
@@ -1,9 +1,17 @@
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class TopTreePotentialTest {
 
     static TopTreeInterface topTree;
+    static ArrayList<ArrayList<Integer>> edges;
+    static Tree tree;
+    static Random rnd;
+
+    static File csv;
+    static PrintWriter pw;
 
     // This class is for testing the potential of top trees.
     // In particular, we wish to test that the potential behaves as expected
@@ -12,61 +20,144 @@ public class TopTreePotentialTest {
 
     public static void main(String[] args) {
 
+        try {
+            // Prepare data logging file
+            csv = new File("testPotential2.csv");
+            pw = new PrintWriter(csv);
+            pw.write("Operation,OperationNumber,Runtime,PotentialChange\n");
+
+
+            int numberOfVertices = 100000;
+            int numberOfEdges = 5000;
+            edges = new ArrayList<>();
+            setupTopTree(numberOfVertices);
+
+            System.out.println("Run: " + 30000 + " " + 4);
+            testPotential(numberOfVertices, 100, 0);
+
+
+            pw.flush();
+            pw.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
 
     }
 
+    private static void setupTopTree(int numberOfVertices) {
+        rnd = new Random();
 
-    private static void testPotential(int numberOfVertices, int numberOfEdge){
-        Random rnd = new Random();
+        tree = Tree.createTree(numberOfVertices);
+        topTree = new SizeTopTree();
 
-        ArrayList<Kruskal.GraphEdge> edges = new ArrayList<>();
-        int j = 0;
-        for (int i = 0; i < numberOfEdge; i++){
-            if (i % (numberOfEdge/10) == 0){
-                j++;
-            }
-            int dest = Math.abs(rnd.nextInt() % numberOfVertices);
-            int source = Math.abs(rnd.nextInt() % numberOfVertices);
+        ArrayList<Integer> unconnected = new ArrayList<>();
+        for (int i = 0; i < numberOfVertices; i++ ){
+            unconnected.add(i);
+        }
+        ArrayList<Integer> connected = new ArrayList<>();
 
-            if (dest == source) {
+        int a = rnd.nextInt(numberOfVertices);
+        int b = (rnd.nextInt(numberOfVertices) + a) % numberOfVertices;
+
+        if (a == b){
+            System.out.println("Unlucky rerun it");
+        }
+
+        topTree.link(tree.vertex.get(a), tree.vertex.get(b), 0);
+        ArrayList<Integer> tempList = new ArrayList<>();
+        tempList.add(a);
+        tempList.add(b);
+        edges.add(tempList);
+        connected.add(a);
+        connected.add(b);
+
+        int i = 0;
+        while(unconnected.size() > 0){
+            System.out.println(i);
+            a = unconnected.get(rnd.nextInt(unconnected.size()));
+            b = connected.get(rnd.nextInt(connected.size()));
+            topTree.link(tree.vertex.get(a), tree.vertex.get(b), 0);
+            tempList = new ArrayList<>();
+            tempList.add(a);
+            tempList.add(b);
+            edges.add(tempList);
+            unconnected.remove(Integer.valueOf(a));
+            connected.add(a);
+            i++;
+        }
+
+    }
+
+
+    private static void testPotential(int numberOfVertices, int expose, int seed){
+        // Test the expose and deexpose operation runtime and potential change
+        for (int i = 0; i < expose; i++){
+            double potentialBefore = computePotentialOfAllRoots(numberOfVertices);
+            int a = rnd.nextInt(numberOfVertices);
+            long start = System.nanoTime();
+            topTree.expose(tree.vertex.get(a));
+            long end = System.nanoTime();
+            double potentialAfter = computePotentialOfAllRoots(numberOfVertices);
+            pw.write("Expose," + i + "," + (end - start) + "," + (potentialAfter - potentialBefore) + "\n");
+            System.out.println("Potential before " + potentialBefore);
+            System.out.println("Potential after " + potentialAfter);
+            potentialBefore = potentialAfter;
+            topTree.deExpose(tree.vertex.get(a));
+        }
+
+        for (int i = 0; i < expose; i++){
+            int a = rnd.nextInt(numberOfVertices);
+            topTree.expose(tree.vertex.get(a));
+            double potentialBefore = computePotentialOfAllRoots(numberOfVertices);
+            long start = System.nanoTime();
+            topTree.deExpose(tree.vertex.get(a));
+            long end = System.nanoTime();
+            double potentialAfter = computePotentialOfAllRoots(numberOfVertices);
+            pw.write("deExpose," + i + "," + (end - start) + "," + (potentialAfter - potentialBefore) + "\n");
+            System.out.println("Potential before " + potentialBefore);
+            System.out.println("Potential after " + potentialAfter);
+        }
+
+
+        ArrayList<ArrayList<Vertex>> EdgesRemoved = new ArrayList<>();
+        // Test cut, and then re-add those in random order
+        for (int i = 0; i < expose; i++){
+            int a = rnd.nextInt(edges.size());
+            if (tree.vertex.get(a).firstEdge == null){
                 i--;
                 continue;
             }
-            int weight = Math.abs(rnd.nextInt() % 100) + 1;
-            edges.add(new Kruskal.GraphEdge(source, dest, weight));
+            Edge e = tree.vertex.get(a).firstEdge;
+            ArrayList<Vertex> list = new ArrayList<>();
+            list.add(e.endpoints[0]);
+            list.add(e.endpoints[1]);
+            EdgesRemoved.add(list);
+            double potentialBefore = computePotentialOfAllRoots(numberOfVertices);
+            long start = System.nanoTime();
+            topTree.cut(e);
+            long end = System.nanoTime();
+            double potentialAfter = computePotentialOfAllRoots(numberOfVertices);
+            pw.write("cut," + i + "," + (end - start) + "," + (potentialAfter - potentialBefore) + "\n");
+            System.out.println("Potential before " + potentialBefore);
+            System.out.println("Potential after " + potentialAfter);
         }
-        Tree t = Tree.createTree(numberOfVertices);
-        SizeTopTree topTree = new SizeTopTree();
 
-        for (int i = 0; i < numberOfEdge; i++) {
-            if (i % (numberOfEdge / 10) == 0) {
-                //System.out.println(10 * j + " percent done");
-                j++;
-            }
-            int a = edges.get(i).src;
-            int b = edges.get(i).dest;
-            int weight = edges.get(i).weight;
-            Node root1 = topTree.expose(t.vertex.get(a));
-            Node root2 = topTree.expose(t.vertex.get(b));
+        // Test cut, and then re-add those in random order
+        for (int i = 0; i < expose; i++){
+            int a = rnd.nextInt(EdgesRemoved.size());
+            ArrayList<Vertex> list = EdgesRemoved.get(a);
+            EdgesRemoved.remove(list);
 
-            LeafNode maxEdge = null;
-            boolean insertLink = false;
-            if (root1 != null && (root1.equals(root2))){
-                // Skip edge
-            } else {
-                insertLink = true;
-            }
-            topTree.deExpose(t.vertex.get(a));
-            topTree.deExpose(t.vertex.get(b));
-            if(insertLink){
-                Node newRoot = topTree.link(t.vertex.get(a), t.vertex.get(b), weight);
-            }
+            double potentialBefore = computePotentialOfAllRoots(numberOfVertices);
+            long start = System.nanoTime();
+            topTree.link(list.get(0), list.get(1), 0);
+            long end = System.nanoTime();
+            double potentialAfter = computePotentialOfAllRoots(numberOfVertices);
+            pw.write("link," + i + "," + (end - start) + "," + (potentialAfter - potentialBefore) + "\n");
+            System.out.println("Potential before " + potentialBefore);
+            System.out.println("Potential after " + potentialAfter);
         }
-        SizeUserInfo userInfo = (SizeUserInfo) topTree.findRoot(t.vertex.get(0).firstEdge.userData).userInfo;
-        int size = userInfo.size;
-        System.out.println("Size of spanning tree is: " + size);
-        topTree.testSplay();
 
     }
 
